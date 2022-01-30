56cd924da52ee8b1640830675b997b0e75e0e636
Nimesh Amin <nimesh@iris-io.com>
2022-01-29 18:17:23 -0800

    WIP for adding Notifi card

--- pages/dao/[symbol]/index.tsx
+++ pages/dao/[symbol]/index.tsx
@@ -16,6 +16,7 @@ import NFTSCompactWrapper from '@components/NFTS/NFTSCompactWrapper'
 import useGovernanceAssets from '@hooks/useGovernanceAssets'
 import useTreasuryAccountStore from 'stores/useTreasuryAccountStore'
 import { usePrevious } from '@hooks/usePrevious'
+import NotificationsCard from '@components/NotificationsCard'
 
 const compareProposals = (p1: Proposal, p2: Proposal) => {
   const p1Rank = p1.getStateSortRank()
@@ -152,6 +153,7 @@ const REALM = () => {
           </div>
         </div>
         <div className="col-span-12 md:col-span-5 lg:col-span-4 space-y-4">
+          <NotificationsCard />
           <TokenBalanceCard />
           <NFTSCompactWrapper></NFTSCompactWrapper>
           <AccountsCompactWrapper />
